7845fdba9bfcf00e262e5251d7e3f547e6a34562
Siroshun09 <siro.shun8@gmail.com>
2022-12-29 13:07:08 +0900

    build: update Box to 5.2.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,9 @@ dependencies {
     implementation("com.github.siroshun09.configapi:configapi-yaml:4.6.0")
     implementation("com.github.siroshun09.translationloader:translationloader:2.0.2")
 
-    compileOnly("net.okocraft.box:box-api:5.1.0-SNAPSHOT")
-    compileOnly("net.okocraft.box:box-storage-api:5.1.0-SNAPSHOT")
-    compileOnly("net.okocraft.box:box-stick-feature:5.1.0-SNAPSHOT")
+    compileOnly("net.okocraft.box:box-api:5.2.0-SNAPSHOT")
+    compileOnly("net.okocraft.box:box-storage-api:5.2.0-SNAPSHOT")
+    compileOnly("net.okocraft.box:box-stick-feature:5.2.0-SNAPSHOT")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
