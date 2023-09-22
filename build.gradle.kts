a59848195bac12a4870aaae9f244697bb0d9b1be
Siroshun09 <siro.shun8@gmail.com>
2023-09-22 19:12:25 +0900

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     java
-    id("io.papermc.paperweight.userdev") version "1.5.5"
+    id("io.papermc.paperweight.userdev") version "1.5.6"
     id("com.github.johnrengelman.shadow") version "8.1.0"
 }
 
@@ -21,11 +21,11 @@ dependencies {
     implementation("com.github.siroshun09.configapi:configapi-yaml:4.6.4")
     implementation("com.github.siroshun09.translationloader:translationloader:2.0.2")
 
-    compileOnly("net.okocraft.box:box-api:5.3.1")
-    compileOnly("net.okocraft.box:box-storage-api:5.3.1")
-    compileOnly("net.okocraft.box:box-stick-feature:5.3.1")
+    compileOnly("net.okocraft.box:box-api:5.4.2")
+    compileOnly("net.okocraft.box:box-storage-api:5.4.2")
+    compileOnly("net.okocraft.box:box-stick-feature:5.4.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
