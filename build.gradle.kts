111339ba278d73a07905addc349bb2953cdfdaf9
Siroshun09 <siro.shun8@gmail.com>
2024-11-01 03:13:14 +0900

    build: update Box API to 6.0.0-rc.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,9 @@ repositories {
 dependencies {
     paperweight.paperDevBundle("$mcVersion-R0.1-SNAPSHOT")
 
-    compileOnly("net.okocraft.box:box-api:6.0.0-rc.1")
-    compileOnly("net.okocraft.box:box-gui-feature:6.0.0-rc.1")
-    compileOnly("net.okocraft.box:box-stick-feature:6.0.0-rc.1")
+    compileOnly("net.okocraft.box:box-api:6.0.0-rc.2")
+    compileOnly("net.okocraft.box:box-gui-feature:6.0.0-rc.2")
+    compileOnly("net.okocraft.box:box-stick-feature:6.0.0-rc.2")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
     testRuntimeOnly("io.papermc.paper:paper-api:$mcVersion-R0.1-SNAPSHOT")
