bdaa7bd9ad58bddfae1fda72b2e0817c24686533
Siroshun09 <siro.shun8@gmail.com>
2025-06-21 14:02:56 +0900

    build: update to Minecraft 1.21.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 group = "net.okocraft.boxtradestick"
 version = "1.6"
 
-val mcVersion = "1.21.5"
+val mcVersion = "1.21.6"
 val fullVersion = "${version}-mc${mcVersion}"
 
 repositories {
