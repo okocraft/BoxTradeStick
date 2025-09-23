9687f5f60eabb29cf3c18a9761cfd7a32bf145eb
Siroshun09 <siro.shun8@gmail.com>
2025-09-23 14:40:53 +0900

    build: update to Minecraft 1.21.9-pre2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 group = "net.okocraft.boxtradestick"
 version = "1.6"
 
-val mcVersion = "1.21.7"
+val mcVersion = "1.21.9-pre2"
 val fullVersion = "${version}-mc${mcVersion}"
 
 repositories {
@@ -43,7 +43,7 @@ tasks {
 
     processResources {
         filesMatching(listOf("paper-plugin.yml")) {
-            expand("projectVersion" to version, "apiVersion" to mcVersion)
+            expand("projectVersion" to version, "apiVersion" to "1.21.9")
         }
     }
 
