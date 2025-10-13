fd76b16db3a13e576feeeaa28109bafa42d7766a
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2025-10-13 19:40:09 +0900

    build: update to Minecraft 1.21.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 group = "net.okocraft.boxtradestick"
 version = "1.6"
 
-val mcVersion = "1.21.9"
+val mcVersion = "1.21.10"
 val fullVersion = "${version}-mc${mcVersion}"
 
 repositories {
